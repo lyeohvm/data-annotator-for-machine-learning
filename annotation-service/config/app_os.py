3dc04f1ad391620184da6cda29ab25664fbdb448
lyeohvm <lyeoh@vmware.com>
2021-07-15 14:25:17 +0800

    add config for mongodb in devops branch to be used in workflows test

---
+++ annotation-service/config/app_os.py
@@ -0,0 +1,24 @@
+# Copyright 2019-2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+app = {
+
+    "MONGODB_URL": os.getenv("MONGODB_URL", "mongodb://mo/daml"),
+    "MONGODB_COLLECTION": os.getenv("MONGODB_COLLECTION", "daml"),
+
+    "USE_LOCAL_FILE_SYS": os.getenv("USE_LOCAL_FILE_SYS", True),
+    "USE_AWS": os.getenv("USE_AWS", False),
+    "REGION": os.getenv("REGION", None),
+    "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID", None),
+    "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY", None),
+
+    "S3_BUCKET_NAME": os.getenv("S3_BUCKET_NAME", None),
+    "S3_ROLE_ARN": os.getenv("S3_ROLE_ARN", None),
+
+    # optional token config
+    "TOKEN_ALGORITHM": os.getenv("TOKEN_ALGORITHM", "HS256"),
+    "TOKEN_SECRET_OR_PRIVATE_KEY": os.getenv("TOKEN_SECRET_OR_PRIVATE_KEY", "OPEN SOURCE"),
+
+
+}
